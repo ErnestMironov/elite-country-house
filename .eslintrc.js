0ac57e4f7e20b2337884454c5c9807524b1ee9d6
Mironov Ernest <erik1297e@gmail.com>
2022-07-29 23:48:41 +0300

    fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,9 @@ module.exports = {
   ],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    "vue/multi-word-component-names": ["error", {
+      "ignores": ["default"]
+    }]
+  },
 }
