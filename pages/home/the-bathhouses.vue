9da93b1033cb67b6bc8d1ee4bb833f68cb4b191c
Mironov Ernest <erik1297e@gmail.com>
2022-08-15 02:03:44 +0300

    add the bathouses block

---
+++ pages/home/the-bathhouses.vue
@@ -0,0 +1,138 @@
+<template>
+  <section class="bathhouses">
+    <div class="flex flex-col items-start justify-between">
+      <div>
+        <SimpleTitle title="Бани и сауны" />
+        <p class="mt-[3rem] max-w-[33.3125rem]">
+          {{ currentHouse.description }}
+        </p>
+        <ul
+          class="grid grid-cols-[repeat(2,_auto)] mt-[3rem] gap-y-[3rem] gap-x-[1.5rem]"
+        >
+          <li class="param mr-[3.75rem]">
+            <span class="param__title">{{ currentHouse.area }}м²</span>
+            <span class="param__value">Площадь бани</span>
+          </li>
+          <li class="mr-[3.125rem] param">
+            <span class="param__title">{{ currentHouse.capacity }}</span>
+            <span class="param__value">Максимальная вместительность</span>
+          </li>
+          <li class="mr-[1.75rem] param">
+            <span class="param__title">{{ currentHouse.poolArea }}м²</span>
+            <span class="param__value">Площадь бассейна</span>
+          </li>
+          <li class="param">
+            <span class="param__title">{{ currentHouse.additioanl }}</span>
+            <span class="param__value">Описание преимущества</span>
+          </li>
+        </ul>
+      </div>
+      <a href="/" class="btn px-[2.9375rem] mt-5">Узнать больше</a>
+    </div>
+    <swiper
+      ref="bathhousesSwiper"
+      class="bathhouses__slider"
+      :options="swiperOptions"
+      @slideChange="changeActiveHouse"
+    >
+      <swiper-slide
+        v-for="image in bathhousesImages"
+        :key="image"
+        class="swiper-slide bathhouses__slide"
+      >
+        <img class="bathhouses__slide-img" :src="image" />
+      </swiper-slide>
+    </swiper>
+  </section>
+</template>
+
+<script>
+import SimpleTitle from '~/components/ui/simple-title/simple-title.vue'
+export default {
+  name: 'TheBathhouses',
+  components: { SimpleTitle },
+  data() {
+    return {
+      activeHouse: 0,
+      objects: [
+        {
+          image:
+            'https://cdnn21.img.ria.ru/images/07e4/06/1a/1573515666_0:161:1476:991_600x0_80_0_0_803c45679d0834b45a6634bfdb6b1687.png',
+
+          description:
+            'Здесь нам необходим текст, который в общих чертах расскажет про то, какие качетсвенные услуги представляет компания. Не забыть упомянуть высокотехнологичность домов и апартаментов и премиальный уровень класса домом. Если если перечень отдыха “не на один день” упомянуть, что в доме не становится скучно на 2й и 3й день.',
+          area: 29,
+          poolArea: 12,
+          capacity: 14,
+          additioanl: 'Пункт/Число',
+        },
+        {
+          image:
+            'https://fainaidea.com/wp-content/uploads/2020/08/depositphotos_9150211-stock-photo-sauna.jpg',
+          description:
+            'Здесь нам необходим текст, который в общих чертах расскажет про то, какие качетсвенные услуги представляет компания. Не забыть упомянуть высокотехнологичность домов и апартаментов и премиальный уровень класса домом. Если если перечень отдыха “не на один день” упомянуть, что в доме не становится скучно на 2й и 3й день.',
+          area: 35,
+          poolArea: 15,
+          capacity: 15,
+          additioanl: 'Пункт/Число',
+        },
+        {
+          image: 'https://www.villa-arte.ru/images/bg-sauna.jpg',
+          description:
+            'Здесь нам необходим текст, который в общих чертах расскажет про то, какие качетсвенные услуги представляет компания. Не забыть упомянуть высокотехнологичность домов и апартаментов и премиальный уровень класса домом. Если если перечень отдыха “не на один день” упомянуть, что в доме не становится скучно на 2й и 3й день.',
+          area: 45,
+          poolArea: 18,
+          capacity: 16,
+          additioanl: 'Пункт/Число',
+        },
+      ],
+      swiperOptions: {
+        slidesPerView: 'auto',
+        loop: true,
+        slideToClickedSlide: true,
+        spaceBetween: 24,
+        grabCursor: true,
+      },
+    }
+  },
+  computed: {
+    currentHouse() {
+      return this.objects[this.activeHouse]
+    },
+    bathhousesImages() {
+      return this.objects.map((object) => object.image)
+    },
+  },
+  methods: {
+    changeActiveHouse() {
+      this.activeHouse = this.$refs.bathhousesSwiper.$swiper.realIndex
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.bathhouses {
+  margin-top: 11.25rem;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 4rem;
+
+  &__slider {
+    min-height: 40.5rem;
+    width: 37.3125rem;
+    margin: 0;
+  }
+
+  &__slide {
+    width: 26.3125rem;
+  }
+
+  &__slide-img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,20 @@
     <TheServices />
     <TheObjects />
     <GuestHouses />
+    <TheBathhouses />
   </main>
 </template>
 
 <script lang="ts">
 import GuestHouses from './home/guest-houses.vue'
+import TheBathhouses from './home/the-bathhouses.vue'
 import TheHero from './home/the-hero.vue'
 import TheObjects from './home/the-objects.vue'
 import TheServices from './home/the-services.vue'
 
 export default {
   name: 'IndexPage',
-  components: { TheHero, TheServices, TheObjects, GuestHouses },
+  components: { TheHero, TheServices, TheObjects, GuestHouses, TheBathhouses },
   head() {
     return {
       title: 'Гедонист',
