a32f0160b32faccd04fb005569e5fad6e973b8bb
Ivan <nekrasov.ivan4@gmail.com>
2022-08-20 17:27:49 +0300

    + guest-house sliders

--- components/blocks/house-parameters/index.vue
+++ components/blocks/house-parameters/index.vue
@@ -1,6 +1,36 @@
 <template>
   <section class="parameters">
-    <div class="swiper-container"></div>
+    <div class="swiper-container">
+       <swiper
+          ref="guestHousesSwiper"
+          class="parameters__slider"
+          :options="swiperOptions"
+        >
+          <swiper-slide
+            class="swiper-slide parameters__slide"
+          >
+            <img class="parameters__slide-img" src="~/assets/images/mock/parameters-slider__1.jpg" />
+          </swiper-slide>
+          <swiper-slide
+            class="swiper-slide parameters__slide"
+          >
+            <img class="parameters__slide-img" src="~/assets/images/mock/parameters-slider__2.jpg" />
+          </swiper-slide>
+          <swiper-slide
+            class="swiper-slide parameters__slide"
+          >
+            <img class="parameters__slide-img" src="~/assets/images/mock/parameters-slider__3.jpg" />
+          </swiper-slide>
+          <!-- <swiper-slide
+            v-for="image in images"
+            :key="image.id"
+            class="swiper-slide parameters__slide"
+          >
+            <img class="parameters__slide-img" :src="image.src" />
+          </swiper-slide> -->
+    </swiper>
+    </div>
+    <div class="parameters__background"></div>
     <div class="parameters-wrapper">
       <Title :title="'Параметры'" is-light></Title>
       <div class="parameters-info">
@@ -26,16 +56,56 @@
   </section>
 </template>
 
-<script setup>
+<script>
 import Title from '@/components/ui/simple-title/simple-title'
 import { createFloorsString } from '@/helpers/helpers'
 
-const mock = {
-  cost: 50000,
-  floors: 2,
-  rooms: 4,
-  square: 145,
+export default{
+  components:{
+    Title
+  },
+  data() {
+    return {
+      activeHouse: 0,
+         images: [
+        {
+        id: 1,
+        src:'https://archello.s3.eu-central-1.amazonaws.com/images/2021/06/29/wizhevsky-architect-modern-country-house-private-houses-archello.1624964046.9447.jpg',
+        },
+        {
+        id: 2,
+        src:'https://st.hzcdn.com/simgs/pictures/exteriors/french-country-house-plan-041-00187-america-s-best-house-plans-img~aad1271f0d2f6da3_4-0055-1-10ade95.jpg',
+
+        },
+        {
+        id: 3,
+        src:'https://cf.bstatic.com/xdata/images/hotel/max1024x768/260533832.jpg?k=23dad23a669810f1d022dc58a00e3455219dfcbc19ed12930217990b178e8b20&o=&hp=1',
+        },
+      ],
+      swiperOptions: {
+        slidesPerView: 'auto',
+        // slidesPerView: '2.2',
+        loop: true,
+        slideToClickedSlide: true,
+        spaceBetween: 16,
+        // navigation: {
+        //   prevEl: '#guestHousesSLiderPrev',
+        //   nextEl: '#guestHousesSliderNext',
+        // },
+      },
+      mock : {
+        cost: 50000,
+        floors: 2,
+        rooms: 4,
+        square: 145,
+      }
+    }
+  },
+  methods: {
+    createFloorsString
+  }
 }
+
 </script>
 
 <style lang="scss" scoped>
--- pages/guest-house/index.vue
+++ pages/guest-house/index.vue
@@ -1,5 +1,20 @@
 <template>
 <div class="house-wrapper">
+
+    <swiper
+          ref="guestHousesSwiper"
+          class="house__slider"
+          :options="swiperOptions"
+        >
+          <swiper-slide
+            v-for="image in images"
+            :key="image.id"
+            class="swiper-slide house__slide"
+          >
+            <img class="house__slide-img" :src="image.src" />
+          </swiper-slide>
+    </swiper>
+
   <section class="container">
     <Booking/>
   </section>
@@ -20,7 +35,7 @@
   </div>
 </template>
 
-<script setup>
+<script>
 import HouseAbout from '@/components/blocks/house-about'
 import HouseName from '@/components/blocks/house-name'
 import HouseParameters from '@/components/blocks/house-parameters'
@@ -29,7 +44,6 @@ import Booking from '@/components/booking'
 
 // import Swiper from 'swiper/swiper-bundle.min'
 // import 'swiper/swiper-bundle.min.css'
-console.log('hello script setup')
 
 // const onSwiper = (swiper) => {
 //       console.log(swiper);
@@ -37,6 +51,44 @@ console.log('hello script setup')
 //     const onSlideChange = () => {
 //       console.log('slide change');
 //     };
+
+export default {
+  components:{
+    HouseAbout,
+    HouseName,
+    HouseParameters,
+    HouseFunctional
+  },
+  data() {
+    return {
+      activeHouse: 0,
+      images: [
+        {
+        id: 1,
+        src:'https://archello.s3.eu-central-1.amazonaws.com/images/2021/06/29/wizhevsky-architect-modern-country-house-private-houses-archello.1624964046.9447.jpg',
+        },
+        {
+        id: 2,
+        src:'https://st.hzcdn.com/simgs/pictures/exteriors/french-country-house-plan-041-00187-america-s-best-house-plans-img~aad1271f0d2f6da3_4-0055-1-10ade95.jpg',
+        },
+        {
+        id: 3,
+        src:'https://cf.bstatic.com/xdata/images/hotel/max1024x768/260533832.jpg?k=23dad23a669810f1d022dc58a00e3455219dfcbc19ed12930217990b178e8b20&o=&hp=1',
+        },
+      ],
+      swiperOptions: {
+        slidesPerView: '3',
+        loop: true,
+        slideToClickedSlide: true,
+        spaceBetween: 16,
+        // navigation: {
+        //   prevEl: '#guestHousesSLiderPrev',
+        //   nextEl: '#guestHousesSliderNext',
+        // },
+      },
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
