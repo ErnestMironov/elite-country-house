e2ea2eab8f2e28ed8e2b800ab802dd16a2d94169
Ivan <nekrasov.ivan4@gmail.com>
2022-08-21 17:39:56 +0300

    + faq

---
+++ pages/FAQ/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="container questions">
+    <SimpleTitle
+    title="Частозадаваемые вопросы"/>
+    <div class="questions-wrapper" >
+      <div v-for="question in questions" :key="question.id" class="questions__item">
+        <h3 class="questions__title">{{question.title}}</h3>
+      </div>
+    </div>
+    <img src="@/assets/images/HEDONIST.svg" alt="" class="questions__bg-img">
+  </section>
+</template>
+
+<script>
+import SimpleTitle from '@/components/ui/simple-title/simple-title.vue';
+
+
+export default {
+  components:{
+   SimpleTitle
+  },
+  data() {
+    return {
+     questions: [
+      {
+        id: 1,
+        title: 'Частозадаваемый вопрос номер N'
+      },
+      {
+        id: 2,
+        title: 'Частозадаваемый вопрос номер N'
+      },
+      {
+        id: 3,
+        title: 'Частозадаваемый вопрос номер N'
+      },
+      {
+        id: 4,
+        title: 'Частозадаваемый вопрос номер N'
+      },
+      {
+        id: 5,
+        title: 'Частозадаваемый вопрос номер N'
+      },
+      {
+        id: 6,
+        title: 'Частозадаваемый вопрос номер N'
+      },
+      {
+        id: 7,
+        title: 'Частозадаваемый вопрос номер N'
+      },
+      {
+        id: 8,
+        title: 'Частозадаваемый вопрос номер N'
+      },
+      {
+        id: 9,
+        title: 'Частозадаваемый вопрос номер N'
+      },
+      {
+        id: 10,
+        title: 'Частозадаваемый вопрос номер N'
+      },
+     ]
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  @import './index.scss';
+</style>
