4b1a18c6937507fee5e51d4ef0d62587d856e890
Mironov Ernest <erik1297e@gmail.com>
2022-12-13 00:44:06 +0600

    Feat: add payment qr code

--- components/booking/precheck.vue
+++ components/booking/precheck.vue
@@ -11,151 +11,185 @@
         ✕
       </button>
       <div class="decoration">HEDONIST</div>
-      <div class="flex justify-between items-center mb-[21px]">
-        <SimpleTitle title="Пречек"></SimpleTitle>
+      <div v-if="!showPayment" class="pb-[326px] flex flex-col items-center">
         <img
-          class="h-[25px] lg:h-[56px]"
+          class="h-[25px] lg:h-[56px] block ml-auto"
           src="~/assets/icons/logo_without-text.svg"
           alt=""
         />
+        <h3 class="text-[18px] text-center font-medium mt-[211px]">
+          Оплатите заказ по {{ $device.isMobile ? 'СБП' : 'QR-коду' }}
+        </h3>
+        <div class="option mt-4 w-[300px] max-w-full mx-auto">
+          <span>К оплате</span>
+          <span>{{ finalPrice | formatPrice }}₽</span>
+        </div>
+        <a
+          v-if="$device.isMobile"
+          :href="paymentLink"
+          target="blank"
+          class="btn text-[20px] my-[40px] mx-auto flex items-center justify-between bg-[#1b1537] py-0 px-[18px]"
+        >
+          <img src="~/assets/icons/sbplogo.png" alt="" class="w-[50%]" />
+          Оплатить
+        </a>
+        <div v-else ref="qrContainer" class="inline-block mt-8"></div>
+        <button
+          class="btn text-[16px] py-[18px] px-[64px] mt-[24px] mx-auto block"
+        >
+          Оплатил
+        </button>
       </div>
-      <div class="pt-[24px] border-t border-solid border-black">
-        <div class="flex">
+      <div v-else>
+        <div class="flex justify-between items-center mb-[21px]">
+          <SimpleTitle title="Пречек"></SimpleTitle>
           <img
-            :src="`https://admin.hedonistclub.ru${order?.objectParams?.images[0]?.url}`"
+            class="h-[25px] lg:h-[56px]"
+            src="~/assets/icons/logo_without-text.svg"
             alt=""
-            class="w-[83px] h-[83px] object-cover mr-[18px]"
           />
-          <div class="flex-1">
-            <h3 class="text-[18px] font-medium">
-              {{ mainObject }} "{{ order?.objectParams?.name }}"
-            </h3>
-            <ul class="flex flex-wrap">
-              <li class="text-[#2D292980] text-[16px] mr-[12px]">
-                {{ mainObjectDuration }}
-              </li>
-              <li class="text-[#2D292980] text-[16px] mr-[12px]">
-                {{ mainObjectPersons }}
-              </li>
-              <li class="text-[#2D292980] text-[16px] mr-[12px]">
-                {{ new Date(order?.from ?? 0).toLocaleDateString() }}
-              </li>
-              <li class="text-[#2D292980] text-[16px] mr-[12px]">
-                {{ order?.arrivalTime | formatOrderTime }}
-              </li>
-            </ul>
-            <div class="option option_without_dots mt-[13px]">
-              <b>Базовая цена</b>
-              <b>{{ order?.totalBasePrice | formatPrice }}₽</b>
+        </div>
+        <div class="pt-[24px] border-t border-solid border-black">
+          <div class="flex">
+            <img
+              :src="`https://admin.hedonistclub.ru${order?.objectParams?.images[0]?.url}`"
+              alt=""
+              class="w-[83px] h-[83px] object-cover mr-[18px]"
+            />
+            <div class="flex-1">
+              <h3 class="text-[18px] font-medium">
+                {{ mainObject }} "{{ order?.objectParams?.name }}"
+              </h3>
+              <ul class="flex flex-wrap">
+                <li class="text-[#2D292980] text-[16px] mr-[12px]">
+                  {{ mainObjectDuration }}
+                </li>
+                <li class="text-[#2D292980] text-[16px] mr-[12px]">
+                  {{ mainObjectPersons }}
+                </li>
+                <li class="text-[#2D292980] text-[16px] mr-[12px]">
+                  {{ new Date(order?.from ?? 0).toLocaleDateString() }}
+                </li>
+                <li class="text-[#2D292980] text-[16px] mr-[12px]">
+                  {{ order?.arrivalTime | formatOrderTime }}
+                </li>
+              </ul>
+              <div class="option option_without_dots mt-[13px]">
+                <b>Базовая цена</b>
+                <b>{{ order?.totalBasePrice | formatPrice }}₽</b>
+              </div>
+            </div>
+          </div>
+          <div
+            v-for="option in objectOptions"
+            :key="option.id"
+            class="option mt-[30px]"
+          >
+            <b
+              >{{ option.title }}
+              {{ option.type === 'number' ? `(×${option.value})` : '' }}</b
+            >
+            <b
+              >{{
+                (option.type === 'number'
+                  ? option.price * option.value
+                  : option.price) | formatPrice
+              }}₽</b
+            >
+          </div>
+          <div class="summary">
+            <div class="option">
+              <b>Итог</b>
+              <b>{{ objectTotalPrice | formatPrice }}₽</b>
             </div>
           </div>
         </div>
-        <div
-          v-for="option in objectOptions"
-          :key="option.id"
-          class="option mt-[30px]"
-        >
-          <b
-            >{{ option.title }}
-            {{ option.type === 'number' ? `(×${option.value})` : '' }}</b
-          >
-          <b
-            >{{
-              (option.type === 'number'
-                ? option.price * option.value
-                : option.price) | formatPrice
-            }}₽</b
-          >
-        </div>
-        <div class="summary">
-          <div class="option">
-            <b>Итог</b>
-            <b>{{ objectTotalPrice | formatPrice }}₽</b>
+        <div v-if="order?.bathhouse_order?.bathhouse" class="pt-[24px] mt-10">
+          <div class="flex">
+            <img
+              :src="`https://admin.hedonistclub.ru${bathParams?.images[0]?.url}`"
+              alt=""
+              class="w-[83px] h-[83px] object-cover mr-[18px]"
+            />
+            <div class="flex-1">
+              <h3 class="text-[18px] font-medium">"{{ bathParams?.name }}"</h3>
+              <ul class="flex flex-wrap">
+                <li class="text-[#2D292980] text-[16px] mr-[12px]">
+                  {{ bathDuration }}
+                </li>
+                <li class="text-[#2D292980] text-[16px] mr-[12px]">
+                  {{ bathPersons }}
+                </li>
+                <li class="text-[#2D292980] text-[16px] mr-[12px]">
+                  {{
+                    new Date(
+                      order?.bathhouse_order?.dateTime ?? 0
+                    ).toLocaleDateString({ timezone: '' })
+                  }}
+                </li>
+                <li class="text-[#2D292980] text-[16px] mr-[12px]">
+                  {{
+                    new Date(
+                      order?.bathhouse_order?.dateTime ?? 0
+                    ).toLocaleTimeString() | formatOrderTime
+                  }}
+                </li>
+              </ul>
+              <div class="option option_without_dots mt-[13px]">
+                <b>Базовая цена</b>
+                <b>{{ order?.bathhouse_order?.basePrice | formatPrice }}₽</b>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div v-if="order?.bathhouse_order?.bathhouse" class="pt-[24px] mt-10">
-        <div class="flex">
-          <img
-            :src="`https://admin.hedonistclub.ru${bathParams?.images[0]?.url}`"
-            alt=""
-            class="w-[83px] h-[83px] object-cover mr-[18px]"
-          />
-          <div class="flex-1">
-            <h3 class="text-[18px] font-medium">"{{ bathParams?.name }}"</h3>
-            <ul class="flex flex-wrap">
-              <li class="text-[#2D292980] text-[16px] mr-[12px]">
-                {{ bathDuration }}
-              </li>
-              <li class="text-[#2D292980] text-[16px] mr-[12px]">
-                {{ bathPersons }}
-              </li>
-              <li class="text-[#2D292980] text-[16px] mr-[12px]">
-                {{
-                  new Date(
-                    order?.bathhouse_order?.dateTime ?? 0
-                  ).toLocaleDateString({ timezone: '' })
-                }}
-              </li>
-              <li class="text-[#2D292980] text-[16px] mr-[12px]">
-                {{
-                  new Date(
-                    order?.bathhouse_order?.dateTime ?? 0
-                  ).toLocaleTimeString() | formatOrderTime
-                }}
-              </li>
-            </ul>
-            <div class="option option_without_dots mt-[13px]">
-              <b>Базовая цена</b>
-              <b>{{ order?.bathhouse_order?.basePrice | formatPrice }}₽</b>
+          <div
+            v-for="option in bathOptions"
+            :key="option.id"
+            class="option mt-[30px]"
+          >
+            <b
+              >{{ option.title }}
+              {{ option.type === 'number' ? `(×${option.value})` : '' }}</b
+            >
+            <b
+              >{{
+                (option.type === 'number'
+                  ? option.price * option.value
+                  : option.price) | formatPrice
+              }}₽</b
+            >
+          </div>
+          <div class="summary">
+            <div class="option">
+              <b>Итог</b>
+              <b>{{ order?.bathhouse_order?.totalPrice | formatPrice }}₽</b>
             </div>
           </div>
         </div>
-        <div
-          v-for="option in bathOptions"
-          :key="option.id"
-          class="option mt-[30px]"
-        >
-          <b
-            >{{ option.title }}
-            {{ option.type === 'number' ? `(×${option.value})` : '' }}</b
-          >
-          <b
-            >{{
-              (option.type === 'number'
-                ? option.price * option.value
-                : option.price) | formatPrice
-            }}₽</b
-          >
-        </div>
-        <div class="summary">
+        <div class="total-summary">
+          <div v-if="!order?.refundable" class="option">
+            <b>Скидка</b>
+            <b>{{ discount | formatPrice }}₽</b>
+          </div>
           <div class="option">
             <b>Итог</b>
-            <b>{{ order?.bathhouse_order?.totalPrice | formatPrice }}₽</b>
+            <b>{{ finalPrice | formatPrice }}₽</b>
           </div>
         </div>
+        <button
+          class="btn text-[16px] py-[18px] px-[64px] mt-[24px] ml-auto block"
+          @click="createOrder"
+        >
+          Перейти к оплате
+        </button>
       </div>
-      <div class="total-summary">
-        <div v-if="!order?.refundable" class="option">
-          <b>Скидка</b>
-          <b>{{ discount | formatPrice }}₽</b>
-        </div>
-        <div class="option">
-          <b>Итог</b>
-          <b>{{ finalPrice | formatPrice }}₽</b>
-        </div>
-      </div>
-      <button
-        class="btn text-[16px] py-[18px] px-[64px] mt-[24px] ml-auto block"
-      >
-        Оплатить
-      </button>
     </div>
   </div>
 </template>
 
 <script>
+import QRCodeStyling from 'qr-code-styling'
 import SimpleTitle from '~/components/ui/simple-title/simple-title.vue'
+import sbpLogo from '~/assets/icons/sbp_logo.svg?url'
 import {
   daysPluralize,
   hoursPluralize,
@@ -181,17 +215,18 @@ export default {
       required: true,
     },
     objectOptions: {
-      type: Object,
+      type: Array,
       required: true,
     },
     bathOptions: {
-      type: Object,
+      type: Array,
       required: true,
     },
   },
   data() {
     return {
       bathParams: null,
+      showPayment: false,
     }
   },
   computed: {
@@ -265,6 +300,10 @@ export default {
         ? this.totalPrice - this.discount
         : this.totalPrice
     },
+
+    paymentLink() {
+      return `https://qr.nspk.ru/AS1A007E0LK6NJ3L8RLAHG0RK82BSAQ9?type=01&bank=100000000008&crc=5D89&sum=${this.finalPrice}00&cur=RUB`
+    },
   },
   async mounted() {
     if (this.order?.bathhouse_order?.bathhouse) {
@@ -274,10 +313,39 @@ export default {
         )
       ).data
     }
+
+    const qrCode = new QRCodeStyling({
+      width: 300,
+      height: 300,
+      type: 'svg',
+      data: this.paymentLink,
+      image: sbpLogo,
+      dotsOptions: {
+        color: '#2D2929',
+        type: 'rounded',
+      },
+      backgroundOptions: {
+        color: 'transparent',
+      },
+      imageOptions: {
+        crossOrigin: 'anonymous',
+        margin: 20,
+      },
+    })
+
+    qrCode.append(this.$refs.qrContainer)
   },
   methods: {
     closePrecheck() {
       this.$emit('onClose')
+    },
+    async createOrder() {
+      try {
+        await this.$http.$post('guest-house-orders', dataToSend)
+        this.showPayment = true
+      } catch (error) {
+        alert(error.message)
+      }
     },
   },
 }
@@ -315,10 +383,10 @@ export default {
 .option {
   display: flex;
 
-  b {
+  b,
+  span {
     font-size: 18px;
     line-height: 24px;
-    font-weight: 500;
 
     &:first-child {
       order: 1;
@@ -327,6 +395,10 @@ export default {
     &:nth-child(2) {
       order: 3;
     }
+  }
+
+  b {
+    font-weight: 500;
   }
 
   &::after {
