2aabb3b3a7c04eca9b62d6f110f8ab844f2beb80
MironovErnest <erik1297e@gmail.com>
2023-09-25 02:48:07 +0200

    feat: add ym

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,31 @@ export default {
         src: 'https://realtycalendar.ru/webpack/application.js?_t=9058a171d7fb2c4af6cb',
         defer: true,
       },
-    ]
+      {
+        type: "text/javascript",
+        innerHTML: `
+          (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+          ym(95005107, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+          });
+        `,
+      }
+    ],
+    noscript: [
+      {
+        innerHTML: `
+        <div><img src="https://mc.yandex.ru/watch/95005107" style="position:absolute; left:-9999px;" alt="" /></div>
+        `,
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
